dc277fe1d3fa327d473a456cf1bc45016d49efb6
ohch4too <mw419@pm.me>
2025-10-09 00:01:27 -0700

    rearrange packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,4 +6,4 @@ dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome
 dnf5 remove -y gnome-tour malcontent-control
 
 flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
-flatpak install -y fedora org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
+flatpak install -y fedora org.gnome.Calculator #org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
