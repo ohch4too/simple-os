7832c037044b3aa1b959215c75e3b480353ff27c
ohch4too <mw419@pm.me>
2025-10-09 12:31:20 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,8 +21,8 @@ dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal flatpak
 #systemd-machine-id-setup
 #export DISPLAY=:0.0
 
-#flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
 #flatpak --noninteractive --system --no-deps -y install fedora org.gnome.Calculator
 
-flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
-flatpak --noninteractive --system --no-deps -y install fedora org.gnome.Calculator
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak --noninteractive --system --no-deps -y install flathub org.gnome.Calculator
