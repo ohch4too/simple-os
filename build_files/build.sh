c4f33f01772c8372c1e98b71a177245d753ac0a9
ohch4too <mw419@pm.me>
2025-10-09 11:27:48 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,14 @@
 
 set -ouex pipefail
 
-dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome-system-monitor nautilus distrobox flatpak flatseal rsync whois net-tools pwgen
-dnf5 remove -y gnome-tour malcontent-control
+#dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome-system-monitor nautilus distrobox flatpak flatseal rsync whois net-tools pwgen
+#dnf5 remove -y gnome-tour malcontent-control
 
-dnf5 install -y gnome-calculator gnome-text-editor gnome-screenshot loupe snapshot virt-manager firefox chromium torbrowser-launcher
+#dnf5 install -y gnome-calculator gnome-text-editor gnome-screenshot loupe snapshot virt-manager firefox chromium torbrowser-launcher
 
 #flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
 #flatpak --noninteractive --system -y install fedora org.gnome.Calculator #org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
+
+dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal flatpak
+flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
+flatpak --noninteractive --system -y install fedora org.gnome.Calculator
