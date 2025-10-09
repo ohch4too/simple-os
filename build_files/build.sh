16a1bc21ad464e93eacbc6c9130b634e082979ac
ohch4too <mw419@pm.me>
2025-10-09 12:39:07 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ dnf5 remove -y gnome-tour malcontent-control
 #dnf5 install -y gnome-calculator gnome-text-editor gnome-screenshot loupe snapshot virt-manager firefox chromium torbrowser-launcher
 
 #flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
-#flatpak --noninteractive --system -y install fedora org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
+#flatpak --system --noninteractive --no-deps -y install fedora org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak --noninteractive --system -y install flathub org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
+flatpak --system --noninteractive --no-deps -y install flathub org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
