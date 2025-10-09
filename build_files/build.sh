20526464f1d70ac7c0c476c28679b041a3132301
ohch4too <mw419@pm.me>
2025-10-08 23:46:46 -0700

    rearrange packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,8 @@
 
 set -ouex pipefail
 
-dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-text-editor gnome-calculator nautilus rsync whois net-tools pwgen terminator firefox chromium torbrowser-launcher @virtualization
-
+dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome-system-monitor nautilus flatpak flatseal rsync whois net-tools pwgen
 dnf5 remove -y gnome-tour malcontent-control
 
+flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+flatpak install -y fedora org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
