a3b46c960d973722192ef1afc59753d4429d1ce7
ohch4too <mw419@pm.me>
2025-10-09 13:49:09 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,13 +2,8 @@
 
 set -ouex pipefail
 
-dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome-system-monitor nautilus distrobox flatpak flatseal rsync whois net-tools pwgen
+dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome-system-monitor gnome-calculator gnome-text-editor gnome-screenshot evince loupe snapshot nautilus flatpak flatseal distrobox rsync whois net-tools pwgen
 dnf5 remove -y gnome-tour malcontent-control
 
-#dnf5 install -y gnome-calculator gnome-text-editor gnome-screenshot loupe snapshot virt-manager firefox chromium torbrowser-launcher
-
-flatpak remote-add --if-not-exists --authenticator-name=org.flatpak.Authenticator.Oci --system fedora oci+https://registry.fedoraproject.org
-flatpak --system --noninteractive --no-deps -y install fedora org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
-
-#flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-#flatpak --system --noninteractive --no-deps -y install flathub org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
+flatpak remote-add --if-not-exists --subset=verified flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak --system --noninteractive --no-deps -y install flathub org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium org.torproject.torbrowser-launcher
