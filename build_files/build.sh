b5893de7b3dfd91493e483d7d4df144deda4444c
ohch4too <mw419@pm.me>
2025-10-09 13:15:08 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,7 +7,7 @@ dnf5 remove -y gnome-tour malcontent-control
 
 #dnf5 install -y gnome-calculator gnome-text-editor gnome-screenshot loupe snapshot virt-manager firefox chromium torbrowser-launcher
 
-flatpak remote-add --if-not-exists --no-authenticator-install --system fedora https://registry.fedoraproject.org
+flatpak remote-add --if-not-exists --no-authenticator-install --system fedora oci+https://registry.fedoraproject.org
 flatpak --system --noninteractive --no-deps -y install fedora org.gnome.Calculator org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
 
 #flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
