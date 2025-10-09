5110688047c300c07f3085f8c2ece5f84b98bef8
ohch4too <mw419@pm.me>
2025-10-09 14:06:46 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,5 +5,5 @@ set -ouex pipefail
 dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome-system-monitor gnome-calculator gnome-text-editor gnome-screenshot evince loupe snapshot nautilus virt-manager flatpak flatseal distrobox rsync whois net-tools pwgen
 dnf5 remove -y gnome-tour malcontent-control
 
-flatpak remote-add --if-not-exists --subset=verified flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak --system --noninteractive --no-deps -y install flathub org.mozilla.firefox org.chromium.Chromium org.torproject.torbrowser-launcher
