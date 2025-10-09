b87d6fde962895a172e0db49e3697ae582185371
ohch4too <mw419@pm.me>
2025-10-08 21:35:35 -0700

    add packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 set -ouex pipefail
+
+dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-text-editor gnome-calculator nautilus rsync whois net-tools pwgen terminator firefox chromium torbrowser-launcher @virtualization
+
+dnf5 remove -y gnome-tour malcontent-control
+
