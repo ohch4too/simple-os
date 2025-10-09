473e9150098501a416f92353d96d1172c9ec174f
ohch4too <mw419@pm.me>
2025-10-09 01:02:47 -0700

    rearrange packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,8 +5,8 @@ set -ouex pipefail
 dnf5 install -y gnome-shell gnome-initial-setup gnome-terminal gnome-boxes gnome-system-monitor nautilus distrobox flatpak flatseal rsync whois net-tools pwgen
 dnf5 remove -y gnome-tour malcontent-control
 
-systemd-machine-id-setup
+#systemd-machine-id-setup
 #mkdir -p /root/.cache/dconf
 
 flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
-flatpak --system -y install --or-update fedora org.gnome.Calculator #org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
+flatpak --noninteractive --system -y install fedora org.gnome.Calculator #org.gnome.TextEditor org.gnome.Loupe org.gnome.Screenshot org.gnome.Snapshot org.gnome.Evince org.virt_manager.virt-manager org.mozilla.firefox org.chromium.Chromium
